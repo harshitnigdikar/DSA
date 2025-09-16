59a2702de57abfdad8fd942445c637e93a1ce77a
Harshit Nigdikar <harshitnigdikar@gmail.com>
2025-09-16 23:34:39 +0530

    LC141LinkListCycle

---
+++ leetcode/blind75/LC141LinkListCycle.java
@@ -0,0 +1,27 @@
+package dsa.leetcode.blind75;
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+public class LC141LinkListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
