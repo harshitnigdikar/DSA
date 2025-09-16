d4ebb8f3b5763c8aaf3aae671fc7fb47eea63e9a
Harshit Nigdikar <harshitnigdikar@gmail.com>
2025-09-16 10:53:46 +0530

    Blind75 started LC1 done

---
+++ leetcode/blind75/LC1.java
@@ -0,0 +1,23 @@
+package dsa.leetcode.blind75;
+
+import java.util.HashMap;
+
+public class LC1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int[] ans = new int[2];
+
+        for(int i = 0; i < nums.length; i++){
+            int compliment = target - nums[i];
+            if(!hm.containsKey(compliment)){
+                hm.put(nums[i], i);
+            }else{
+                int idx = hm.get(compliment);
+                ans[0] = i;
+                ans[1] = idx;
+                return ans;
+            }
+        }
+        return ans;
+    }
+}
