5964b2184cf327a5670e78d2720827650f5ca5bb
Harshit Nigdikar <harshitnigdikar@gmail.com>
2025-09-25 00:29:14 +0530

    LC1129ShortestPathWithAlternateColors

--- leetcode/graphs/practice/LC1129ShortestPathWithAlternateColors.java
+++ leetcode/graphs/practice/LC1129ShortestPathWithAlternateColors.java
@@ -27,7 +27,6 @@ public class LC1129ShortestPathWithAlternateColors {
 
         for (int i = 0; i < edges.length; i++) {
             adjList.get(edges[i][0]).add(edges[i][1]);
-
         }
         return adjList;
     }
@@ -37,9 +36,9 @@ public class LC1129ShortestPathWithAlternateColors {
         ArrayList<ArrayList<Integer>> blueAdjList = createList(blueEdges,n);
 
         Queue<Pair> queue = new LinkedList<>();
-        boolean visR[] = new boolean[n];
-        boolean visB[] = new boolean[n];
-        int ans[] = new int[n];
+        boolean[] visR = new boolean[n];
+        boolean[] visB = new boolean[n];
+        int[] ans = new int[n];
         Arrays.fill(ans,-1); // Initializing with -1 assuming till now no node is rechable
         queue.add(new Pair(0,0,0)); // Add 0 as red edge
         queue.add(new Pair(0,1,0)); // Add 0 as blue edge
@@ -64,7 +63,6 @@ public class LC1129ShortestPathWithAlternateColors {
                         if(!visR[neigh]){
                             visR[neigh] = true;
                             queue.add(new Pair(neigh,0,dist+1));
-
                         }
                     }
                 }else if(color == 0){ // if red then checking in blueList
@@ -72,12 +70,10 @@ public class LC1129ShortestPathWithAlternateColors {
                         if(!visB[neigh]){
                             visB[neigh] = true;
                             queue.add(new Pair(neigh,1,dist+1));
-
                         }
                     }
                 }
             }
-
             dist++;
         }
         return ans;
