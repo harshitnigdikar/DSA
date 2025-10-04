3fed59749854dc0495a5e7c396f05d2a9ad18d54
Harshit Nigdikar <harshitnigdikar@gmail.com>
2025-10-04 23:50:06 +0530

    Trying jump game

---
+++ leetcode/blind75/LC55JumpGame.java
@@ -0,0 +1,16 @@
+package dsa.leetcode.blind75;
+
+import java.util.HashMap;
+
+public class LC55JumpGame {
+    public boolean canJump(int[] nums) {
+
+    }
+    public boolean solve(int idx, int[] nums, HashMap<Integer, Integer> hm){
+
+        for (int i = 0; i < nums[idx]; i++) {
+
+        }
+
+    }
+}
